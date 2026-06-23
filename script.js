9fa81c91169ca21562c6a8bf0a5dada6da7dac96
nguyenhoangdien9x-lang <nguyenhoangdien9x@gmail.com>
2026-06-23 20:45:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ document.addEventListener('DOMContentLoaded', function() {
     // Thay đường link bằng Web App URL của thư mục Báo Hỷ vừa tạo ở Bước 1
     const scriptUrlBaoHy = 'DÁN_LINK_APPS_SCRIPT_CỦA_BÁO_HỶ_VÀO_ĐÂY';
     loadImagesFromDrive('grid-baohy', scriptUrlBaoHy);
+    // 1C. Kích hoạt lấy ảnh từ Google Drive cho Lễ Vu Quy
+    // Thay đường link bằng Web App URL của thư mục Vu Quy
+    const scriptUrlVuQuy = 'https://script.google.com/macros/s/AKfycbykqcOb80j2LbIr2_i5lXq57su8q_uk_tYCSuhOD6jY3BhAQI0Sz61L8lbHrXu9Ecc/exec';
+    loadImagesFromDrive('grid-vuquy', scriptUrlVuQuy);
 
     // 2. Kích hoạt hiệu ứng cuộn trang (Reveal) lần đầu
     initScrollReveal();
