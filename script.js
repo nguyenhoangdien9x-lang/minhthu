d5dca3ee23cd8c84fda435973826aff23edd0349
nguyenhoangdien9x-lang <nguyenhoangdien9x@gmail.com>
2026-06-23 22:10:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // BẠN THÊM 2 DÒNG NÀY CHO TIỆC BÁO HỶ NHÉ:
     // Thay đường link bằng Web App URL của thư mục Báo Hỷ vừa tạo ở Bước 1
-    const scriptUrlBaoHy = 'DÁN_LINK_APPS_SCRIPT_CỦA_BÁO_HỶ_VÀO_ĐÂY';
+    const scriptUrlBaoHy = 'https://script.google.com/macros/s/AKfycbzVRKmv8PBQ1z8x2lzhGhafoX-WsZdGYplf-emJ2CimrKjC0Kr-oWm-ijqFfA8Z_KfC/exec';
     loadImagesFromDrive('grid-baohy', scriptUrlBaoHy);
     // 1C. Kích hoạt lấy ảnh từ Google Drive cho Lễ Vu Quy
     // Thay đường link bằng Web App URL của thư mục Vu Quy
-    const scriptUrlVuQuy = 'https://script.google.com/macros/s/AKfycbykqcOb80j2LbIr2_i5lXq57su8q_uk_tYCSuhOD6jY3BhAQI0Sz61L8lbHrXu9Ecc/exec';
+    const scriptUrlVuQuy = 'https://script.google.com/macros/s/AKfycbyfI2BN8GPl4F236Bg1Kz0dUu9oM-VGf20n-LYU8cWkf_CgpU_A1XU8mMjMNHSrkgO6Vw/exec';
     loadImagesFromDrive('grid-vuquy', scriptUrlVuQuy);
 
     // 2. Kích hoạt hiệu ứng cuộn trang (Reveal) lần đầu
