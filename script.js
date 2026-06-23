171afdd19310ea2c7bef2facdc349e89e11c7549
nguyenhoangdien9x-lang <nguyenhoangdien9x@gmail.com>
2026-06-23 20:10:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,10 @@ document.addEventListener('DOMContentLoaded', function() {
     // 1. Kích hoạt lấy ảnh từ Google Drive cho Lễ Tân Hôn
     const scriptUrlTanHon = 'https://script.google.com/macros/s/AKfycbzjdu7Hq_YnfOUkKtg-Ol8tpON1Iw-zjMciupMlzIIaIs_tCd8fyPgnzMjLgf_AXs8/exec';
     loadImagesFromDrive('grid-tanhon', scriptUrlTanHon);
-
+// BẠN THÊM 2 DÒNG NÀY CHO TIỆC BÁO HỶ NHÉ:
+    // Thay đường link bằng Web App URL của thư mục Báo Hỷ vừa tạo ở Bước 1
+    const scriptUrlBaoHy = 'https://script.google.com/macros/s/AKfycbxJsJNxmPb1cHCZJvMxo76MrNicbK9jYYImbAWrR0cgK0xPkm3J1UldrjsRDghAHQuB/exec';
+    loadImagesFromDrive('grid-baohy', scriptUrlBaoHy);
     // 2. Kích hoạt hiệu ứng cuộn trang (Reveal) lần đầu
     initScrollReveal();
 
