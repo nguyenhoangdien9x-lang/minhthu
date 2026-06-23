4195881acaf0a10f84f5d16ff85bbc6f02de4caf
nguyenhoangdien9x-lang <nguyenhoangdien9x@gmail.com>
2026-06-23 22:18:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,19 @@
 let allWishes = [];
 let displayedCount = 0;
 const wishesPerPage = 5; // Số lời chúc hiện mỗi lần bấm xem thêm
+
 // === CHẠY KHI TRANG WEB VỪA TẢI XONG ===
 document.addEventListener('DOMContentLoaded', function() {
     
-    // 1. Kích hoạt lấy ảnh từ Google Drive cho Lễ Tân Hôn
+    // 1A. Kích hoạt lấy ảnh từ Google Drive cho Lễ Tân Hôn
     const scriptUrlTanHon = 'https://script.google.com/macros/s/AKfycbzjdu7Hq_YnfOUkKtg-Ol8tpON1Iw-zjMciupMlzIIaIs_tCd8fyPgnzMjLgf_AXs8/exec';
     loadImagesFromDrive('grid-tanhon', scriptUrlTanHon);
 
-    // BẠN THÊM 2 DÒNG NÀY CHO TIỆC BÁO HỶ NHÉ:
-    // Thay đường link bằng Web App URL của thư mục Báo Hỷ vừa tạo ở Bước 1
+    // 1B. Kích hoạt lấy ảnh từ Google Drive cho Tiệc Báo Hỷ
     const scriptUrlBaoHy = 'https://script.google.com/macros/s/AKfycbzVRKmv8PBQ1z8x2lzhGhafoX-WsZdGYplf-emJ2CimrKjC0Kr-oWm-ijqFfA8Z_KfC/exec';
     loadImagesFromDrive('grid-baohy', scriptUrlBaoHy);
+
     // 1C. Kích hoạt lấy ảnh từ Google Drive cho Lễ Vu Quy
-    // Thay đường link bằng Web App URL của thư mục Vu Quy
     const scriptUrlVuQuy = 'https://script.google.com/macros/s/AKfycbyfI2BN8GPl4F236Bg1Kz0dUu9oM-VGf20n-LYU8cWkf_CgpU_A1XU8mMjMNHSrkgO6Vw/exec';
     loadImagesFromDrive('grid-vuquy', scriptUrlVuQuy);
 
